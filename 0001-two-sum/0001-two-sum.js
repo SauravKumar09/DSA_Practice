67a4612a7d902ac1c1c19cf20f8bd37cad1e2bd8
Saurav Kumar <90619704+SauravKumar09@users.noreply.github.com>
2026-06-23 17:23:19 +0530

    Time: 63 ms (11.05%), Space: 55.9 MB (20.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,26 +5,27 @@
  */
 var twoSum = function(nums, target) {
     // **** Two Pointers Approach
-    // for(let i=0; i<=nums.length; i++){
-    //     for (let j=i+1; j<=nums.length; j++){
-    //         if (nums[i] + nums[j] === target){
-    //             return [i, j];
-    //         }
-    //     }
-    // }
+    for(let i=0; i<=nums.length; i++){
+        for (let j=i+1; j<=nums.length; j++){
+            if (nums[i] + nums[j] === target){
+                return [i, j];
+            }
+        }
+    }
 
-    // return [];
+    return [];
 
 
     //**** HashMap Approach  */ 
     // Time complexity: O(n)
     // Space complexity: O(n)
-    const map = new Map();
-    for(let i=0; i<nums.length; i++){
-        const comp = target - nums[i];
-        if(map.has(comp)){
-            return [map.get(comp), i];
-        }
-        map.set(nums[i], i);
-    }
+
+    // const map = new Map();
+    // for(let i=0; i<nums.length; i++){
+    //     const comp = target - nums[i];
+    //     if(map.has(comp)){
+    //         return [map.get(comp), i];
+    //     }
+    //     map.set(nums[i], i);
+    // }
 };
